fe09704246641c0415d125625bd7818cc3c13bec
Daniel Zeh <dfzeh1@gmail.com>
2024-02-08 21:45:07 -0600

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
-function App() {
+function App(props) {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
       <div className="card">
         <div className="top">
-          <h2  className="name">Beyonce</h2>
+          <h2  className="name">{props.name}</h2>
           <img className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
+            src={props.image}
+            alt={props.alt}
           />
         </div>
         <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
+          <p className="info">{props.number}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+
+<div> 
+<h1 className="heading">My Contacts</h1>
+<App name="Beyonce" image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" number="+123 456 789" email="b@beyonce.com" />
+<App name="Jack Bauer" image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" number="+987 654 321" email="jack@nowhere.com" />
+
+<App name="Chuck Norris" image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" number="+918 372 574" email="gmail@chucknorris.com" />
+
+
+
+
+
+
+
+
+</div>
+, document.getElementById("root"));
 
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
